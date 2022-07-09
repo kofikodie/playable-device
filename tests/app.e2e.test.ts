cadf40bd71ebec84e7d8a230ca22306bdde1ed94
kody <kofi.addo@hinto.com>
2022-07-09 16:07:12 +0200

    test: husky

--- tests/app.e2e.test.ts
+++ tests/app.e2e.test.ts
@@ -3,6 +3,6 @@ import app from '../src/app';
 
 describe('E2E tests for the app', () => {
     it('should return a valid response for GET /health', () => {
-        return request(app).get('/health').expect(200).expect('OK');
+        return request(app).get('/health').expect(200).expect('OKs');
     });
 });
